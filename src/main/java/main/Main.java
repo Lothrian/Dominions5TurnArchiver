b46f67fb81dba25c1a84e44cd9cab0eef6e83be4
Rafael Fiebig-Bindner <R.Fiebig.Bindner@gmail.com>
2021-01-07 22:59:13 +0100

    Rework project structure

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.dominions5turnarchiver;
+package main;
 
-import static com.mycompany.dominions5turnarchiver.Main.logWriter;
+import static main.Main.logWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
